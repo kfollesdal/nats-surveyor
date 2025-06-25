fe684742359d6f1367ee82385f512b7c4a78f6c8
kfollesdal <kristoffer.follesdal@eviny.no>
2025-06-25 19:08:22 +0200

    Add last_active conusmer timestamp to prometheus

--- surveyor/collector_statz.go
+++ surveyor/collector_statz.go
@@ -1326,6 +1326,8 @@ func (sc *StatzCollector) Describe(ch chan<- *prometheus.Desc) {
 			ch <- sc.descs.accJszConsumerNumPending
 			ch <- sc.descs.accJszConsumerAckFloorStreamSeq
 			ch <- sc.descs.accJszConsumerAckFloorConsumerSeq
+			ch <- sc.descs.accJszConsumerAckFloorLastActive
+			ch <- sc.descs.accJszConsumerDeliveredLastActive
 		}
 	}
 
@@ -1659,6 +1661,24 @@ func (sc *StatzCollector) Collect(ch chan<- prometheus.Metric) {
 							if sc.jszFilterSet[ConsumerAckFloorConsumerSeq] || !hasFilters {
 								metrics.newGaugeMetric(sc.descs.accJszConsumerAckFloorConsumerSeq,
 									consumerStat.consumerAckFloorConsumerSeq,
+									append(accLabels, streamStat.accountName,
+										streamStat.clusterName, raftGroup, streamStat.serverID, streamStat.serverName,
+										streamStat.streamName, streamStat.streamLeader, consumerStat.consumerName, consumerStat.consumerLeader,
+									),
+								)
+							}
+							if sc.jszFilterSet[ConsumerAckFloorLastActive] || !hasFilters {
+								metrics.newGaugeMetric(sc.descs.accJszConsumerAckFloorLastActive,
+									consumerStat.consumerAckFloorLastActive,
+									append(accLabels, streamStat.accountName,
+										streamStat.clusterName, raftGroup, streamStat.serverID, streamStat.serverName,
+										streamStat.streamName, streamStat.streamLeader, consumerStat.consumerName, consumerStat.consumerLeader,
+									),
+								)
+							}
+							if sc.jszFilterSet[ConsumerDeliveredLastActive] || !hasFilters {
+								metrics.newGaugeMetric(sc.descs.accJszConsumerDeliveredLastActive,
+									consumerStat.consumerDeliveredLastActive,
 									append(accLabels, streamStat.accountName,
 										streamStat.clusterName, raftGroup, streamStat.serverID, streamStat.serverName,
 										streamStat.streamName, streamStat.streamLeader, consumerStat.consumerName, consumerStat.consumerLeader,
